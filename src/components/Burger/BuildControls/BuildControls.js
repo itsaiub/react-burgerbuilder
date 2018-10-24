df280a450cddb5177acf29eeef988506ad0e627c
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-24 14:15:24 +0600

    create BuildControl Component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BuildControl from './BuildControl/BuildControl'
+import './BuildControls.css'
+
+const BuildControls = props => {
+  return (
+    <div className='BuildControls'>
+
+    </div>)
+};
+
+export default BuildControls;
