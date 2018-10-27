a6f085609fb39d65edacf620555d228a692a580e
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-28 00:47:34 +0600

    navigation back and to next page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import React, { Component } from "react";
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
 export class Checkout extends Component {
   state = {
@@ -9,15 +9,27 @@ export class Checkout extends Component {
       cheese: 1,
       bacon: 1
     }
-  }
-  
+  };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default Checkout
+export default Checkout;
