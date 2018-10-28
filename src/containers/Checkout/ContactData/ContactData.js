aa92cdc672310e247a968ef9e4fe1aa6db6bbe6d
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-28 23:06:02 +0600

    fix common validation bug

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -50,8 +50,8 @@ export class ContactData extends Component {
         value: "",
         validation: {
           required: true,
-          minLength: 5,
-          maxLength: 9
+          minLength: 4,
+          maxLength: 6
         },
         valid: false
       },
@@ -107,15 +107,15 @@ export class ContactData extends Component {
   };
 
   checkValidity = (value, rules) => {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
     if(rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
     return isValid;
   };
