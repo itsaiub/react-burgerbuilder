e986dcc241bf574313ab8444564d6faa9c45005e
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-31 23:38:21 +0600

    getting token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,28 +1,42 @@
 import * as actionTypes from "./actionTypes";
-
+import Axios from "axios";
 
 export const authStart = () => {
-   return {
-      type: actionTypes.AUTH_START
-   }
-}
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
 
-export const authSuccess = (authData) => {
-   return {
-      type: actionTypes.AUTH_SUCCESS,
-      authData
-   }
-}
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData
+  };
+};
 
-export const authFail = (error) => {
-   return {
-      type: actionTypes.AUTH_FAIL,
-      error
-   }
-}
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error
+  };
+};
 
 export const auth = (email, password) => {
-   return dispatch => {
-      dispatch(authStart())
-   }
-}
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    Axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA-4bfBpGnieeiSrukp-47sD7Jk3QXKW-Y", authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
+  };
+};
