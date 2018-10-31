c16488072a5bc1744db7534a4f6e08b6b47114bf
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-10-31 22:54:54 +0600

    adding authentication form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,7 @@ const NavigationItems = () => {
   return <ul className="NavigationItems">
     <NavigationItem exact link='/'>Burger Builder</NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
+    <NavigationItem link='/auth'>Sign In</NavigationItem>
   </ul>
 }
 
