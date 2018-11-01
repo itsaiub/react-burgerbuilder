867a66075c6699ca64b98abb148dec1edbc7a2cb
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-11-01 10:43:12 +0600

    log out user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,6 +1,20 @@
 import * as actionTypes from "./actionTypes";
 import Axios from "axios";
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expireTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expireTime * 1000);
+  };
+};
+
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
@@ -30,15 +44,18 @@ export const auth = (email, password, isSignup) => {
       password: password,
       returnSecureToken: true
     };
-     let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA-4bfBpGnieeiSrukp-47sD7Jk3QXKW-Y"
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA-4bfBpGnieeiSrukp-47sD7Jk3QXKW-Y";
 
-    if(!isSignup) {
-       url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyA-4bfBpGnieeiSrukp-47sD7Jk3QXKW-Y'
+    if (!isSignup) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyA-4bfBpGnieeiSrukp-47sD7Jk3QXKW-Y";
     }
     Axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(error => {
         console.log(error);
