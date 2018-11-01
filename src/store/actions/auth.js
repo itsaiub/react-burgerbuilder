c1e9af73e56bd9436299bf3cc8e877674a07156d
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-11-01 16:19:47 +0600

    log in after refresh the page

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,6 +2,9 @@ import * as actionTypes from "./actionTypes";
 import Axios from "axios";
 
 export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("localId");
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -54,6 +57,12 @@ export const auth = (email, password, isSignup) => {
     Axios.post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem("token", response.data.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        localStorage.setItem("localId", response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -64,9 +73,27 @@ export const auth = (email, password, isSignup) => {
   };
 };
 
-export const setAuthRedirectPath = (path) => {
+export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
-  }
-}
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate > new Date()) {
+        const localId = localStorage.getItem("localId");
+        dispatch(authSuccess(token, localId));
+        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000))
+      } else {
+        dispatch(logout());
+      }
+    }
+  };
+};
